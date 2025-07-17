2ff242a86b0a346b51fa9689179914ceeafe9a5c
iiabhi <abhishek.kumar002525@gmail.com>
2025-07-18 00:01:12 +0530

    cf

--- cf.cpp
+++ cf.cpp
@@ -1,15 +1,45 @@
 // #include <bits/stdc++.h>
 // using namespace std;
 
-// int main() {
+// int main(){
 //     ios::sync_with_stdio(false);
 //     cin.tie(nullptr);
 
-//     int t;
-//     cin >> t;
-//     while (t--) {
-       
+//     int T;
+//     cin >> T;
+//     while(T--){
+//         int n;
+//         long long cur;
+//         cin>>n>>cur;
+//         vector<array<long long,3>> a(n);
+//         for(int i = 0; i < n; i++){
+//             cin>>a[i][0]>>a[i][1]>>a[i][2];
+//         }
+//         sort(a.begin(), a.end());
+//         priority_queue<pair<long long,long long>> pq;
+//         int idx = 0;
+//         while(idx < n && a[idx][0] <= cur){
+//             if(a[idx][1] >= cur){
+//                 pq.emplace(a[idx][2], a[idx][1]);
+//             } 
+//             idx++;
+//         }
+//         while(!pq.empty()){
+//             auto p = pq.top(); 
+//             pq.pop();
+//             long long x = p.first;
+//             if(x <= cur){
+//                 break;
+//             } 
+//             cur = x;
+//             while(idx < n && a[idx][0] <= cur){
+//                 if(a[idx][1] >= cur){
+//                     pq.emplace(a[idx][2], a[idx][1]);
+//                 } 
+//                 idx++;
+//             }
+//         }
+//         cout<<cur<<endl;
 //     }
-
 //     return 0;
 // }
